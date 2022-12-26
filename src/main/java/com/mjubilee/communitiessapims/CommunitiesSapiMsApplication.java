dcde5b5b3ae7693abeebd034bfa7f3eca682e9d3
MJ <mahalungguj@gmail.com>
2022-12-25 21:33:43 -0800

    Initialize springboot project

---
+++ src/main/java/com/mjubilee/communitiessapims/CommunitiesSapiMsApplication.java
@@ -0,0 +1,13 @@
+package com.mjubilee.communitiessapims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunitiesSapiMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunitiesSapiMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mjubilee/communitiessapims/CommunitiesSapiMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjubilee.communitiessapims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunitiesSapiMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
